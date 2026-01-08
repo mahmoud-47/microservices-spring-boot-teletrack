3a2c60d74b5465a231d45d6d528089228c7f1ae3
mahmoud-47 <gysoft.sn@gmail.com>
2026-01-08 02:20:45 +0200

    feat(user-service): add service basic folder

---
+++ services/user-service/src/main/java/com/teletrack/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teletrack.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
