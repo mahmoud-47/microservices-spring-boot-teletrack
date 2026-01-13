5fe30724ed84f8f992edc707a903074b37c9d5d9
mahmoud-47 <gysoft.sn@gmail.com>
2026-01-13 09:18:06 +0200

    feat: add mongo + redis config

--- services/reporting-service/src/main/java/com/teletrack/reportingservice/ReportingServiceApplication.java
+++ services/reporting-service/src/main/java/com/teletrack/reportingservice/ReportingServiceApplication.java
@@ -2,8 +2,10 @@ package com.teletrack.reportingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ReportingServiceApplication {
 
 	public static void main(String[] args) {
